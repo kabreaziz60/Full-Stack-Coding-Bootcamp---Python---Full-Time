35e42b3993076d024b11cb5fa95e289d3154fd06
kabreaziz60 <kabreaziz60@gmail.com>
2022-08-26 09:55:10 +0000

    ExerciceXPOR exo 1

---
+++ Week2/Days1/ExerciceXPOr/Exercice1.js
@@ -0,0 +1,13 @@
+// Exercice 1 : Couleur Préférée
+// Des Instructions
+
+// let sentence = ["my","favorite","color","is","blue"];
+// Écrivez un code Javascript simple qui rejoindra tous les éléments du tableau ci-dessus, et console.log le résultat.
+
+
+let sentence = ["my","favorite","color","is","blue"];
+
+for(i==0; i< sentence.length; i+1){
+    var resultar=console.log([i]);
+
+}
