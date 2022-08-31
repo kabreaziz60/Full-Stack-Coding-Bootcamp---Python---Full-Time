2e864689b91ab3578df561e3f110a9b11b76b365
kabreaziz60 <kabreaziz60@gmail.com>
2022-08-31 09:54:30 +0000

    ExerciceXP exo1 terminé

--- Week2/Days1/Exercises XP Ninja/Exercice3.js
+++ Week2/Days1/Exercises XP Ninja/Exercice3.js
@@ -11,3 +11,13 @@
 //     "I love the movie named Nemo" ➞ "I found Nemo at 5"
 //     "Nemo is a cute fish" ➞ "I found Nemo at 0"
 //     "My fish is called Nemo, I love it" ➞ "I found Nemo at 4"
+//-------------------------------------
+
+let users = prompt("Enter une phrase contenant Nemo",0);
+let Rch = users.search("Nemo")
+        console.log(users[Rch]);
+        if(Rch != -1){
+            console.log("I found Nemo at "+ Rch);
+        } else{
+            console.log("Je ne trouve pas Nemo")
+        }
