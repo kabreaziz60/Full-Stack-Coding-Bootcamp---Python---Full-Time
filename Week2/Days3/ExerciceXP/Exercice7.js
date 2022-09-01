94db1b7017b29d48a7344e5b4c280c0c122af79f
kabreaziz60 <kabreaziz60@gmail.com>
2022-09-01 09:15:14 +0000

    ExerciceXOr exo 1 terminer

--- Week2/Days3/ExerciceXP/Exercice7.js
+++ Week2/Days3/ExerciceXP/Exercice7.js
@@ -5,17 +5,15 @@
 // Indice : une chaîne est un tableau de lettres
 // Console.log le nom de leur société secrète. La sortie doit être "ABJKPS"
 //----------------------------------------------------------------
-
 let names = ["Jack", "Philip", "Sarah", "Amanda", "Bernard", "Kyle"];
     var createur = names.sort()
     console.log(names)
     for(let i in names){
         var lettres = names[i][0];
         console.log(lettres);
-    }
-   
+    } 
+    
     console.log(lettres);
-
-    console.log("**************************************")
+    console.log("**************************************");
     
 
